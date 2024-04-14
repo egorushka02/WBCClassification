72a7907ef92d2e6ba53715981096bedbb9698846
Ushakov Egor <usakovegor2002@gmail.com>
2024-04-14 11:35:14 +0300

    feat: added config and utils

---
+++ config.py
@@ -0,0 +1,11 @@
+"""
+    Contains some hyperparameters
+"""
+
+# Setup hyperparameters
+NUM_EPOCHS = 20
+LEARNING_RATE = 0.0001
+
+# Setup directories
+TRAIN_DIR = "data/train"
+VAL_DIR = "data/val"
